571683da56e2c1c87ded35e3e231e89c2168154b
Pereira, Leandro <leandro.pereira1337@gmail.com>
2020-08-05 20:38:48 -0300

    Add get product endpoint with repository pattern

---
+++ SupermarketApi/Repositories/DependencyInjection/RepositoryRegistrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SupermarketApi.Repositories.DependencyInjection
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+    using SupermarketApi.Repositories;
+
+    public static class RepositoryRegistrationExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection serviceCollection)
+        {
+            _ = serviceCollection ?? throw new ArgumentNullException(nameof(serviceCollection));
+
+            return serviceCollection.AddTransient<IProductRepository, ProductRepository>();
+        }
+    }
+}
---
+++ SupermarketApi/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace SupermarketApi.Repositories
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using SupermarketApi.Data;
+    using SupermarketApi.Entities;
+
+    internal sealed class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext storeContext;
+
+        public ProductRepository(StoreContext storeContext)
+        {
+            this.storeContext = storeContext;
+        }
+
+        async Task<Product?> IProductRepository.GetProduct(int id)
+        {
+            return await this.storeContext.Products.FindAsync(id).ConfigureAwait(false);
+        }
+
+        async Task<IReadOnlyCollection<Product>> IProductRepository.GetProducts()
+        {
+            return await this.storeContext.Products
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+    }
+}
