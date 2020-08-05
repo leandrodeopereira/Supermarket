c1281af200da461651c821d72022090d29281a91
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-05 15:07:38 -0300

    Add entity framework migration on project

---
+++ SuperMarketApi/Data/DependencyInjection/DataRegistrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SuperMarketApi.Data.DependencyInjection
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DataRegistrationExtensions
+    {
+        public static IServiceCollection AddDataInfrastruture(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            _ = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _ = serviceCollection ?? throw new ArgumentNullException(nameof(serviceCollection));
+
+            return serviceCollection
+                .AddDbContext<StoreContext>(x => x.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
