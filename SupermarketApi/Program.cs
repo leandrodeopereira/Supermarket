a0ed3a48ce085efa4b5f31fb8f2d0262f76b8a5c
Pereira, Leandro <leandro.pereira1337@gmail.com>
2020-08-07 15:37:38 -0300

    Set the migrations and DB creation at app startup

--- SupermarketApi/Program.cs
+++ SupermarketApi/Program.cs
@@ -1,15 +1,41 @@
 namespace SupermarketApi
 {
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using SupermarketApi.Data;
 
     [ExcludeFromCodeCoverage]
-    public static class Program
+    public sealed class Program
     {
-        public static void Main(string[] args)
+        [SuppressMessage("Globalization", "CA1303: Do not pass literals as localized parameters", Justification = "Library is not localized.")]
+        [SuppressMessage("Design", "CA1031: Do not catch general exception types", Justification = "For now, unknown exceptions.")]
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var contex = services.GetRequiredService<StoreContext>();
+                    await contex.Database.MigrateAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during the migration.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
