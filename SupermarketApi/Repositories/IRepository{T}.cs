c1166632c86016000c97f5527dc8236fe4f7d77c
Pereira, Leandro <leandro.pereira1337@gmail.com>
2020-08-07 20:31:11 -0300

    Create generic repository pattern

---
+++ SupermarketApi/Repositories/IRepository{T}.cs
@@ -0,0 +1,14 @@
+﻿namespace SupermarketApi.Repositories
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using SupermarketApi.Entities;
+
+    public interface IRepository<T>
+        where T : BaseEntity
+    {
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+    }
+}
