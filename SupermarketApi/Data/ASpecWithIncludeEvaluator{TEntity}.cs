43e4a6c331e98d8adea6c950be0a1ebc63bc33c5
Pereira, Leandro <leandro.pereira1337@gmail.com>
2020-08-08 16:21:00 -0300

    Create specification with include expression

---
+++ SupermarketApi/Data/ASpecWithIncludeEvaluator{TEntity}.cs
@@ -0,0 +1,30 @@
+﻿namespace SupermarketApi.Data
+{
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+    using SupermarketApi.Entities;
+    using SupermarketApi.Specifications;
+
+    internal static class ASpecWithIncludeEvaluator<TEntity>
+        where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(
+            IQueryable<TEntity> inputQuery,
+            ASpecWithInclude<TEntity> spec)
+        {
+            _ = inputQuery ?? throw new System.ArgumentNullException(nameof(inputQuery));
+            _ = spec ?? throw new System.ArgumentNullException(nameof(spec));
+
+            var query = inputQuery;
+
+            if (spec.Expression != null)
+            {
+                query = query.Where(spec.Expression);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
---
+++ SupermarketApi/Specifications/ASpecWithInclude.cs
@@ -0,0 +1,17 @@
+﻿namespace SupermarketApi.Specifications
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using NSpecifications;
+
+    public abstract class ASpecWithInclude<T> : ASpec<T>
+    {
+        public ICollection<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            this.Includes.Add(includeExpression);
+        }
+    }
+}
