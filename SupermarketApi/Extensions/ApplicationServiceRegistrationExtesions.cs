47c0ef735cbc196300fe18446b6d9d45c6be4c0e
Pereira, Leandro <leandro.pereira1337@gmail.com>
2020-08-09 04:26:00 -0300

    Clean up the startup class

---
+++ SupermarketApi/Extensions/ApplicationServiceRegistrationExtesions.cs
@@ -0,0 +1,32 @@
+﻿namespace SupermarketApi.Extensions
+{
+    using System;
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+    using SupermarketApi.Errors;
+    using SupermarketApi.Repositories.DependencyInjection;
+
+    public static class ApplicationServiceRegistrationExtesions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            _ = services ?? throw new ArgumentNullException(nameof(services));
+
+            return services
+                .AddRepositories()
+                .Configure<ApiBehaviorOptions>(options =>
+                {
+                    options.InvalidModelStateResponseFactory = actionContext =>
+                    {
+                        var errors = actionContext.ModelState
+                            .Where(e => e.Value.Errors.Count > 0)
+                            .SelectMany(x => x.Value.Errors)
+                            .Select(x => x.ErrorMessage).ToArray();
+
+                        return new BadRequestObjectResult(new ApiValidationErrorResponse(errors));
+                    };
+                });
+        }
+    }
+}
---
+++ SupermarketApi/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace SupermarketApi.Extensions
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.OpenApi.Models;
+
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            _ = services ?? throw new ArgumentNullException(nameof(services));
+
+            return services
+                .AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Supermarket API", Version = "v1" }));
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder applicationBuilder)
+        {
+            _ = applicationBuilder ?? throw new ArgumentNullException(nameof(applicationBuilder));
+
+            return applicationBuilder
+                .UseSwagger()
+                .UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Supermarket API v1"));
+        }
+    }
+}
