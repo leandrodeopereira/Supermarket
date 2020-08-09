224c1fc9d881dccf5bc4e9fa6f4115db1ac5e1bb
Pereira, Leandro <leandro.pereira1337@gmail.com>
2020-08-08 23:43:40 -0300

    Use static file for open product images

--- SupermarketApi/Profiles/MappingProfiles.cs
+++ SupermarketApi/Profiles/MappingProfiles.cs
@@ -10,7 +10,8 @@
         {
             _ = this.CreateMap<Product, ProductDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
         }
     }
 }
