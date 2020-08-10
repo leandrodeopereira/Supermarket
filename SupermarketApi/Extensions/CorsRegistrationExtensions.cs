a7511d4c704cb01f0fa804397d76c73ed1a6daaa
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-10 14:31:33 -0300

    Add cors policy to allow client url as origin

---
+++ SupermarketApi/Extensions/CorsRegistrationExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace SupermarketApi.Extensions
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    [ExcludeFromCodeCoverage]
+    public static class CorsRegistrationExtensions
+    {
+        private const string CorsPolicy = "CorsPolicy";
+
+        public static IServiceCollection AddClientCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+        {
+            _ = services ?? throw new ArgumentNullException(nameof(services));
+            _ = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            return
+                services.AddCors(opt =>
+                {
+                    opt.AddPolicy(CorsPolicy, policy =>
+                    {
+                        _ = policy
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins(configuration["ClientUrl"]);
+                    });
+                });
+        }
+
+        public static IApplicationBuilder UseClientCorsPolicy(this IApplicationBuilder applicationBuilder)
+        {
+            _ = applicationBuilder ?? throw new ArgumentNullException(nameof(applicationBuilder));
+
+            return applicationBuilder.UseCors(CorsPolicy);
+        }
+    }
+}
