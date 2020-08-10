33ab6eb01d034be6251a2bd2ea8e0e5e031e2fe9
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-10 00:02:09 -0300

    Support paging on Specification

--- SupermarketApi/Specifications/ASpecWithQueryOperations{T}.cs
+++ SupermarketApi/Specifications/ASpecWithQueryOperations{T}.cs
@@ -9,13 +9,26 @@
     {
         public ICollection<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
+        public bool IsPagingEnabled { get; private set; } = false;
+
         public Expression<Func<T, object>>? OrderBy { get; set; }
 
         public Expression<Func<T, object>>? OrderByDescending { get; set; }
 
+        public int Skip { get; private set; }
+
+        public int Take { get; private set; }
+
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             this.Includes.Add(includeExpression);
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            this.Skip = skip;
+            this.Take = take;
+            this.IsPagingEnabled = true;
         }
     }
 }
