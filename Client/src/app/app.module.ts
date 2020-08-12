ff41c148f5ff838dd36fbd47c390bfd5b457f6c8
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-11 22:21:39 -0300

    Create CoreModule to handle sigletons as 'NavBar'

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
