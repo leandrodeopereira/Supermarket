b7f67bb8447bb96056e4df7744e775ab7f3ed7c4
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-12 15:30:58 -0300

    Get brand and type to show on shop page

--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from './shop.service';
 import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IProductType } from '../shared/models/productType';
 
 @Component({
   selector: 'app-shop',
@@ -8,14 +10,40 @@ import { IProduct } from '../shared/models/product';
   styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  brands: IBrand[];
   products: IProduct[];
+  productTypes: IProductType[];
 
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getBrands();
+    this.getProducts();
+    this.getProductTypes();
+  }
+
+  getBrands(): void{
+    this.shopService.getBrands().subscribe(
+      response => {
+      this.brands = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getProducts(): void{
     this.shopService.getProducts().subscribe(
       response => {
       this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getProductTypes(): void{
+    this.shopService.getProductTypes().subscribe(
+      response => {
+      this.productTypes = response;
     }, error => {
       console.log(error);
     });
