38b8a95e82dbd59ac0a1d6f6cd592ef11542daf8
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-13 16:02:55 -0300

    Create simple home page with rounting

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -10,6 +11,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     CommonModule,
     FontAwesomeModule,
+    RouterModule,
   ],
   exports: [NavBarComponent]
 })
