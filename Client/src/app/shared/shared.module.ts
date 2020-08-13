ec8fb5b6c0a46af6ce95a23d09d5bdc4932a2383
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-13 13:41:11 -0300

    Extract pagination to a new Pager component

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent],
+  declarations: [
+    PagingHeaderComponent,
+    PagerComponent
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
   exports: [
-    PaginationModule,
     PagingHeaderComponent,
+    PagerComponent,
   ]
 })
 export class SharedModule { }
