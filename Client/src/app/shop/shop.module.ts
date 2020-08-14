ea28dfd8b117fd09363743e3f0305e27e9cf2ff3
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-13 22:31:13 -0300

    Create product details page

--- Client/src/app/shop/shop.module.ts
+++ Client/src/app/shop/shop.module.ts
@@ -3,15 +3,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [
+    ShopComponent,
+    ProductItemComponent,
+    ProductDetailsComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
+    RouterModule,
     SharedModule,
   ],
   exports: [ShopComponent]
