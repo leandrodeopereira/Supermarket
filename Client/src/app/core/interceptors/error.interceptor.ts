c5938562f1cc9c0223817a9bd59e24b5b7dde0fd
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-16 01:12:28 -0300

    Use ngx toastr for 400s errors

--- Client/src/app/core/interceptors/error.interceptor.ts
+++ Client/src/app/core/interceptors/error.interceptor.ts
@@ -9,18 +9,28 @@ import { Router } from '@angular/router';
 
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error) => {
         if (error) {
+          if (error.status === 400){
+            if (error.error.error){
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+
+          if (error.status === 401){
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
+
           if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
