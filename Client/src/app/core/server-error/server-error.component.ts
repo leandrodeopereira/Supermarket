ca16671a8494fd23c063f7def464381be7d92935
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-16 01:39:28 -0300

    Improve server error page with NavigationExtras

--- Client/src/app/core/server-error/server-error.component.ts
+++ Client/src/app/core/server-error/server-error.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
 })
 export class ServerErrorComponent implements OnInit {
-  constructor() {}
+  error: any;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation &&
+      navigation.extras &&
+      navigation.extras.state &&
+      navigation.extras.state.error;
+  }
 
   ngOnInit(): void {}
 }
