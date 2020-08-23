0c0f3d56a0edc3811bd5672c93333e26211147f3
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-22 23:48:53 -0300

    Configure identity services on startup

--- SupermarketApi/Identity/DependencyInjection/IdentityInfrastrutureRegistrationExtensions.cs
+++ SupermarketApi/Identity/DependencyInjection/IdentityInfrastrutureRegistrationExtensions.cs
@@ -2,9 +2,11 @@
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using SupermarketApi.Entities.Identity;
 
     [ExcludeFromCodeCoverage]
     public static class IdentityInfrastrutureRegistrationExtensions
@@ -14,8 +16,15 @@
             _ = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _ = serviceCollection ?? throw new ArgumentNullException(nameof(serviceCollection));
 
+            var builder = serviceCollection.AddIdentityCore<AppUser>();
+
+            builder = new IdentityBuilder(builder.UserType, builder.Services);
+            _ = builder.AddEntityFrameworkStores<AppIdentityDbContext>();
+            _ = builder.AddSignInManager<SignInManager<AppUser>>();
+
             return serviceCollection
-                .AddDbContext<AppIdentityDbContext>(x => x.UseSqlite(configuration.GetConnectionString("IdentityConnection")));
+                .AddDbContext<AppIdentityDbContext>(x => x.UseSqlite(configuration.GetConnectionString("IdentityConnection")))
+                .AddAuthentication().Services;
         }
     }
 }
