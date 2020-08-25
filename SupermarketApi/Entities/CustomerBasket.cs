4630866a407ea8dfa59df13f60c8bf7a537056e3
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-25 00:08:09 -0300

    Resolve CA2227 using IReadOnlyCollection

--- SupermarketApi/Entities/CustomerBasket.cs
+++ SupermarketApi/Entities/CustomerBasket.cs
@@ -2,7 +2,6 @@
 namespace SupermarketApi.Entities
 {
     using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
 
     public sealed class CustomerBasket
     {
@@ -17,7 +16,6 @@ namespace SupermarketApi.Entities
 
         public string Id { get; set; }
 
-        [SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "For now, it will be used as a dto too.")]
-        public ICollection<BasketItem> Items { get; set; } = new List<BasketItem>();
+        public IReadOnlyCollection<BasketItem> Items { get; set; } = new List<BasketItem>();
     }
 }
