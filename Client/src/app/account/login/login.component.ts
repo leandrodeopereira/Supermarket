04b02706d3cba7425233c97d5a1faf9ebf9acb1b
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-08-30 00:25:17 -0300

    Redirect to shop when successfully logged in

--- Client/src/app/account/login/login.component.ts
+++ Client/src/app/account/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ import { AccountService } from '../account.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -28,6 +29,7 @@ export class LoginComponent implements OnInit {
     this.accountService.login(this.loginForm.value).subscribe(
       () => {
         console.log('user logged in');
+        this.router.navigateByUrl('/shop');
       },
       (error) => {
         console.log(error);
