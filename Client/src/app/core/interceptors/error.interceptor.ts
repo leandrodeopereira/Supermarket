bb9fe23a73d24238381642385a7233f247bf2e0b
leandrodeopereira <leandro.pereira1337@gmail.com>
2020-09-04 00:00:56 -0300

    Throw error model instead of HTTP error on interceptor

--- Client/src/app/core/interceptors/error.interceptor.ts
+++ Client/src/app/core/interceptors/error.interceptor.ts
@@ -41,7 +41,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           }
         }
 
-        return throwError(error);
+        return throwError(error.error);
       })
     );
   }
