d05d86e7de9fe5a1139bafcbc3023becf269afcb
leandrodeopereira <leandro.pereira1337@gmail.com>
2021-02-02 22:22:03 -0300

    Reset address form after successfully updated

--- Client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ Client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { IAddress } from 'src/app/shared/models/address';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from 'src/app/account/account.service';
 
@@ -20,8 +21,9 @@ export class CheckoutAddressComponent implements OnInit {
     this.accountService
       .updateUserAddress(this.checkoutForm.get('addressForm').value)
       .subscribe(
-        () => {
+        (address: IAddress) => {
           this.toastr.success('Address saved');
+          this.checkoutForm.get('addressForm').reset(address);
         },
         (error) => {
           this.toastr.error(error.message);
